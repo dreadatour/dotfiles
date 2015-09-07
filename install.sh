7b4e755754b9326b89065970ebcf4dd868a3c8e9
Vladimir Rudnyh <rudnyh@corp.mail.ru>
2015-09-07 18:33:18 +0300

    Update install script

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 if [ ! -d "$HOME/.git" ]; then
     echo "Installing dotfiles:"
     tmpdir=`mktemp -d -t dotfiles.XXXXXXXX`
-    git clone --depth=1 https://github.com/dreadatour/dotfiles.git "$tmpdir"
-    ls -1A "$tmpdir" | while read fd; do
-    	mv "$tmpdir/$fd" $HOME/
+    git clone -q --depth=1 https://github.com/dreadatour/dotfiles.git $tmpdir
+    ls -1A $tmpdir | while read fd; do
+    	cp -r $tmpdir/$fd $HOME/
     	echo "- $fd"
     done
+    rm -rf $tmpdir
 else
     echo "Dotfiles are already installed"
 fi
