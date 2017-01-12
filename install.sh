42871efefe88922644e108f2c83793bb97185de5
Vladimir Rudnyh <dreadatour@gmail.com>
2017-01-12 19:45:31 +0300

    Move 'install.sh' script to separate branch

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ ! -d "$HOME/.git" ]; then
+    echo "Installing dotfiles:"
+    tmpdir=`mktemp -d -t dotfiles.XXXXXXXX`
+    git clone -q --recursive https://github.com/dreadatour/dotfiles.git $tmpdir
+    ls -1A $tmpdir | while read fd; do
+        cp -r $tmpdir/$fd $HOME/
+        echo "- $fd"
+    done
+    rm -rf $tmpdir
+
+    echo "Insalling Vim plugins"
+    vim +PluginInstall +qall
+else
+    echo "Dotfiles are already installed"
+fi
